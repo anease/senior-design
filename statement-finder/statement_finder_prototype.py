2b5830b9d17ec4f5f552b98067c0a20ba6eed2f8
Daniel Wood - Desktop <wooddj@mail.uc.edu>
2021-01-21 14:39:19 -0500

    Refactored code for readability and conciseness

--- statement-finder/statement_finder_prototype.py
+++ statement-finder/statement_finder_prototype.py
@@ -27,85 +27,87 @@ sampleCompSentence = "Cheetahs can run up to 80 miles per hour and grow to be 16
 # This portion of the tool will take strings/arrays of text as input, apply natural language processing to them, and output a list of statements
 # Subtasks: Split text into sentences, use dataset to train model, use trained model to predict each sentence as a statement/nonstatement
 
-import nltk
-import string
-import re
+import nltk, string, re
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import precision_recall_fscore_support as score
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 
-# sentences = nltk.sent_tokenize(sampleText)
+#Process input_text from webpage parser
 input_text = pd.DataFrame(nltk.sent_tokenize(sampleText))
-# input_text.columns = ['input_sentence']
 input_text.columns = ['body_text']
 input_text['label'] = ['undecided' for i in range(input_text.size)]
 test_size = input_text.size // 2
-# print("Test size: ", test_size)
-# print(sentences)
-# print(input_text)
 
-stopwords = nltk.corpus.stopwords.words('english')
-ps = nltk.PorterStemmer()
-wn = nltk.WordNetLemmatizer()
+stopwords = nltk.corpus.stopwords.words('english') #Read in stopwords for easy access later
+wn = nltk.WordNetLemmatizer() #Instantiate lemmatizer
 
+#Read in training data from dataset file
 # data = pd.read_csv('dataset.txt', sep='|')
-# data = pd.read_csv('dataset-binary.txt', sep='|')
-data = pd.read_csv('dataset-bin2.txt', sep='|')
+data = pd.read_csv('dataset-binary.txt', sep='|')
 data.columns = ['label', 'body_text']
-# pd.set_option('max_columns', 18)
-# pd.set_option('max_colwidth', 10)
-# pd.set_option('display.width', 0)
+train_size = data.size // 2
+data = data.append(input_text, ignore_index=True) #Add the input text to the training data
 
-def clean_text(text):
+def clean_text(text): #Method for cleaning text in preparation for vectorizing
     text = "".join([word.lower() for word in text if word not in string.punctuation])
     tokens = re.findall('\w+', text)
     text = [wn.lemmatize(word) for word in tokens if word not in stopwords]
     return text
 
-# tfidf_vect = TfidfVectorizer(analyzer=clean_text)
-# tfidf_vect_fit = tfidf_vect.fit(x_train['body_text'])
-# tfidf_train = tfidf_vect_fit.transform(x_train['body_text'])
-# tfidf_test = tfidf_vect_fit.transform(x_test['body_text'])
+input_text['clean_text'] = [clean_text(text) for text in input_text['body_text']] #Debug line
 
-train_size = data.size // 2
-data = data.append(input_text, ignore_index=True)
-
+#Vectorize the dataset that includes training and testing data
 tfidf_vect = TfidfVectorizer(analyzer=clean_text)
 x_tfidf = tfidf_vect.fit_transform(data['body_text'])
-# x_tfidf = tfidf_vect.fit_transform(data['body_text'], input_text['body_text'])
 x_tfidf_feat = pd.DataFrame(x_tfidf.toarray())
-# print(x_tfidf)
-# print(x_tfidf_feat)
-# print(data)
-# print(train_size)
 
-# #for comparing count vector to tfidf vector
-# count_vect = CountVectorizer(analyzer=clean_text)
-# x_count = count_vect.fit_transform(data['body_text'])
-# x_count_feat = pd.DataFrame(x_count.toarray())
-
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import precision_recall_fscore_support as score
-
-# x_feat = pd.concat
+#Split the data into x/y train/test sets
 # x_train, x_test, y_train, y_test = train_test_split(x_tfidf_feat, data['label'], test_size=0.2)
 x_train = x_tfidf_feat[0:train_size]
 y_train = data['label'][0:train_size]
 x_test = x_tfidf_feat[-test_size:]
 y_test = data['label'][-test_size:]
-# print(x_train)
-# print(y_train)
-# print(x_test)
-# print(y_test)
-# print(data['body_text'][0:10:2])
 
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.model_selection import GridSearchCV
+#Training the Model
+rf = RandomForestClassifier(n_estimators=450, max_depth=25, n_jobs=-1)
+rf_model = rf.fit(x_train, y_train)
+
+#Predictions
+y_pred = rf_model.predict(x_test)
+returned_statements = []
+for i in range(test_size):
+    input_text['label'][i] = y_pred[i]
+    if y_pred[i] == "statement":
+        returned_statements.append(input_text['body_text'][i])
+
+#Results
+print(input_text)
+print("Returned statements: ", returned_statements)
+
+
+#---------------------------------------------------------------------------#
+# Model Comparison and Grid Search for finding parameters
+#---------------------------------------------------------------------------#
+
+# #Preparing the vector for the training/testing
+# tfidf_vect = TfidfVectorizer(analyzer=clean_text)
+# tfidf_vect_fit = tfidf_vect.fit(x_train['body_text'])
+# tfidf_train = tfidf_vect_fit.transform(x_train['body_text'])
+# tfidf_test = tfidf_vect_fit.transform(x_test['body_text'])
+
+# #For comparing count vector to tfidf vector
+# count_vect = CountVectorizer(analyzer=clean_text)
+# x_count = count_vect.fit_transform(data['body_text'])
+# x_count_feat = pd.DataFrame(x_count.toarray())
+
 
 # #Random Forest Classifier
 # #Seems to have slightly higher accuracy than GBClassifier
 # print("RandomForestClassifier:")
-rf = RandomForestClassifier(n_estimators=450, max_depth=25, n_jobs=-1)
-rf_model = rf.fit(x_train, y_train)
+# rf = RandomForestClassifier(n_estimators=450, max_depth=25, n_jobs=-1)
+# rf_model = rf.fit(x_train, y_train)
 # #Predict for the test set
 # y_pred = rf_model.predict(x_test)
 # precision, recall, fscore, support = score(y_test, y_pred, pos_label='statement', average='binary')
@@ -121,6 +123,7 @@ rf_model = rf.fit(x_train, y_train)
 
 
 # #Grid Search model tests
+
 # #Random Forest Grid Search
 # rf = RandomForestClassifier()
 # param_rf = {'n_estimators': [400, 500, 600], 'max_depth': [25]}
@@ -134,20 +137,4 @@ rf_model = rf.fit(x_train, y_train)
 # gs_gb = GridSearchCV(gb, param_gb, cv=5, n_jobs=-1)
 # cv_fit = gs_gb.fit(x_tfidf_feat, data['label'])
 # print(pd.DataFrame(cv_fit.cv_results_).sort_values('mean_test_score', ascending=False)[0:5])
-
-
-
-# #Predictions
-y_pred = rf_model.predict(x_test)
-returned_statements = []
-for i in range(test_size):
-    input_text['label'][i] = y_pred[i]
-    if y_pred[i] == "statement":
-        returned_statements.append(input_text['body_text'][i])
-
-
-print(y_pred)
-print(input_text)
-print("Returned statements: ", returned_statements)
-
 
