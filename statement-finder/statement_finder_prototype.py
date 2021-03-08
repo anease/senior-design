67919a9d806acb3e31f7ed97f1b2b4f7283349f1
wooddj <wooddj@mail.uc.edu>
2021-03-08 13:53:27 -0500

    expanded statement identification

--- statement-finder/statement_finder_prototype.py
+++ statement-finder/statement_finder_prototype.py
@@ -2,7 +2,7 @@
 ## Description: This tool takes text as input, replcaes pronouns with their antecedents, and returns statements that meet dependency relationship criteria.
 
 
-import nltk, string, spacy, neuralcoref
+import nltk, string, spacy, neuralcoref, re
 import pandas as pd #Not necessary for final version
 
 #---------------------------------------------------------------------------#
@@ -86,13 +86,14 @@ def atomic_find_statements(text):
     sentences = nltk.sent_tokenize(resolved_text)
     clauses = []
     for sent in sentences: #split sentences into clauses
-        for clause in split_clauses(sent):
-            clauses.append(clause)
+        if sent[-1] != "?": #filter out sentences ending with a ?
+            for clause in split_clauses(sent):
+                clauses.append(clause)
     for sentence in clauses: #search for statements within each clause
         chunks = []
         root = ""
+        tags = ""
         doc = nlp(sentence)
-        # print(sentence)
         for token in doc: #find and store the root word in the clause
             # print("Word: {}, Word tag: {}, Dependency: {}, Depends on: {}".format(token.text, token.pos_, token.dep_, token.head.text))
             if token.dep_ == "ROOT":
@@ -104,6 +105,7 @@ def atomic_find_statements(text):
         chunks = get_dependencies(root, doc) #get words that depend on the root word (includes root)
         ordered_chunks = []
         for word in chunks: #get chunks from clauses
+            tags = tags + " " + word.dep_
             if word.dep_ == "ROOT":
                 chunk = [word]
                 ordered_chunks.append(chunk)
@@ -111,93 +113,82 @@ def atomic_find_statements(text):
                 chunk = get_chunk(word, doc)
                 ordered_chunks.append(order_chunk(chunk, doc))
             # print("Word: {} || Dependency: {} || Chunk: {}".format(word, word.dep_, chunk))
-            # print("Ordered chunk: {}".format(ordered_chunk))
+            # print("Ordered chunk: {}".format(order_chunk(chunk, doc)))
         statement = ""
         for chunk in ordered_chunks:
             for word in chunk:
                 statement = statement + " " + word.text
         statement = statement[1:]
         
+        # print("Sentence: {}".format(sentence))
+        # print("Tags: {}".format(tags))
+        r1 = re.compile(".*nsubj\s(\w+\s)*ROOT\s(\w+\s)*(dobj)*(advmod)*(acomp)*(attr)*(prep)*(ccomp)*")
+        r2 = re.compile(".*npadvmod\s(\w+\s)*ROOT\s(\w+\s)*(dobj)*(advmod)*(acomp)*(attr)*(prep)*(ccomp)*")
+        r3 = re.compile(".*nsubjpass\s(\w+\s)*ROOT\s(\w+\s)*(dobj)*(advmod)*(acomp)*(attr)*(prep)*(ccomp)*")
+        r4 = re.compile(".*expl\s(\w+\s)*ROOT\s(\w+\s)*(dobj)*(advmod)*(acomp)*(attr)*(prep)*(ccomp)*")
+
         #Return statements if they meet the structure of a statement
-        #Refactor to account for small differences in chunk order (qualifier first, etc)
-        if chunks[0].dep_ == "nsubj" and chunks[1].dep_ == "ROOT" and chunks[2].dep_ == "dobj":
-            statements.append(statement)
-        if chunks[0].dep_ == "nsubj" and chunks[1].dep_ == "ROOT" and chunks[2].dep_ == "advmod":
-            statements.append(statement)
-        if chunks[0].dep_ == "nsubj" and chunks[1].dep_ == "ROOT" and chunks[2].dep_ == "acomp":
-            statements.append(statement)
+        if chunks[0].dep_ != "aux" and chunks[0].dep_ != "advmod":
+            if "like" not in statement and "favorite" not in statement: #poor attempt at removing opinions, will also remove statements that contain those words
+                if len(list(filter(r1.match, [tags]))) > 0:
+                    statements.append(statement)
+                elif len(list(filter(r2.match, [tags]))) > 0:
+                    statements.append(statement)
+                elif len(list(filter(r3.match, [tags]))) > 0:
+                    statements.append(statement)
+                elif len(list(filter(r4.match, [tags]))) > 0:
+                    statements.append(statement)
     return statements
 
 #---------------------------------------------------------------------------#
 # Testing Block for Atomic Statement Finder
 #---------------------------------------------------------------------------#
 
-# #Initial test sentences for atomic_statement_find (previously dependency_grouper)
-# test1 = "Granny Smith apples are green. Do you like Granny Smith apples?"
-# test2 = "Good morning! Who might you be? Sit up straight!"
-# test3 = "Timmy owns 3 different cars. His Honda Civic has good gas mileage."
-# test4 = "Cheetahs run very fast. They run fast because of evolution."
-# test5 = "I like pizza. Pepperoni is my favorite pizza topping."
-# test6 = "Cheetahs move very quickly and turtles move slowly."
-# test7 = "Because of evolution, cheetahs can run very fast."
-# print(dependency_grouper(test1))
-# print(dependency_grouper(test2))
-# print(dependency_grouper(test3))
-# print(dependency_grouper(test4))
-# print(dependency_grouper(test5)) #error: returning one opinion statement
-# print(dependency_grouper(test6))
-# print(dependency_grouper(test7)) #error: not returning statement
+#input: none
+#output: results of running the tests on the data
+def run_tests():
+    # Test 1: [noun] is [adjective]
+    test1 = "Granny Smith apples are green. Do you like Granny Smith apples?"
+    output1 = ["granny smith apples are green"]
+    if atomic_find_statements(test1) == output1:
+        print("Test 1 successful")
+    else:
+        print("Test 1 failed")
+
+    # Test 2: Questions, commands, and exclamations
+    test2 = "Good morning! Who might you be? Sit up straight!"
+    output2 = []
+    if atomic_find_statements(test2) == output2:
+        print("Test 2 successful")
+    else:
+        print("Test 2 failed")
+
+    # Test 3: [noun] has [object]
+    test3 = "Timmy owns 3 different cars. His Honda Civic has good gas mileage."
+    output3 = ["timmy owns 3 different cars", "timmy honda civic has good gas mileage"]
+    if atomic_find_statements(test3) == output3:
+        print("Test 3 successful")
+    else:
+        print("Test 3 failed")
+
+    # Test 4: [subject] does [action] | [subject] causes [action]
+    test4 = "Cheetahs run very fast. They run fast because of evolution."
+    output4 = ["cheetahs run very fast", "cheetahs run fast because of evolution"]
+    if atomic_find_statements(test4) == output4:
+        print("Test 4 successful")
+    else:
+        print("Test 4 failed")
+
+    # Test 5: Opinions
+    test5 = "I like pizza. Pepperoni is my favorite pizza topping."
+    output5 = []
+    if atomic_find_statements(test5) == output5:
+        print("Test 5 successful")
+    else:
+        print("Test 5 failed")
+    return
+
+run_tests()
 
 
-# Test 1: [noun] is [adjective]
-test1 = "Granny Smith apples are green. Do you like Granny Smith apples?"
-output1 = ["granny smith apples are green"]
-if atomic_find_statements(test1) == output1:
-    print("Test 1 successful")
-else:
-    print("Test 1 failed")
-
-# Test 2: Questions, commands, and exclamations
-test2 = "Good morning! Who might you be? Sit up straight!"
-output2 = []
-if atomic_find_statements(test2) == output2:
-    print("Test 2 successful")
-else:
-    print("Test 2 failed")
-
-# Test 3: [noun] has [object]
-test3 = "Timmy owns 3 different cars. His Honda Civic has good gas mileage."
-output3 = ["timmy owns 3 different cars", "timmy honda civic has good gas mileage"]
-if atomic_find_statements(test3) == output3:
-    print("Test 3 successful")
-else:
-    print("Test 3 failed")
-
-# Test 4: [subject] does [action] | [subject] causes [action]
-test4 = "Cheetahs run very fast. They run fast because of evolution."
-output4 = ["cheetahs run very fast", "cheetahs run fast because of evolution"]
-if atomic_find_statements(test4) == output4:
-    print("Test 4 successful")
-else:
-    print("Test 4 failed")
-
-# Test 5: Opinions
-test5 = "I like pizza. Pepperoni is my favorite pizza topping."
-output5 = []
-if atomic_find_statements(test5) == output5:
-    print("Test 5 successful")
-else:
-    print("Test 5 failed")
-
-
-
-# # Block for testing tool on dataset sentences
-# # data = pd.read_csv('dataset-nonstatements.txt', sep='|')
-# data = pd.read_csv('dataset-statements.txt', sep='|')
-# data.columns = ['label', 'body_text']
-# for sentence in data['body_text'][0:10]:
-#     # print("Original sentence: {}".format(sentence))
-#     # print("Statements: {}".format(atomic_find_statements(sentence)))
-#     dependency_grouper(sentence)
-#     print("#"*100)
 
