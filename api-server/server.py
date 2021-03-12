55d3019139d3436598fc6cb32882680872236afa
wooddj <wooddj@mail.uc.edu>
2021-03-12 10:33:22 -0500

    added instructions for statement_finder.py

--- api-server/server.py
+++ api-server/server.py
@@ -13,3 +13,10 @@ def factCheck():
             return '\nFACT!\n'
         else:
             return '\nFALSE\n'
+
+
+## statement_finder.py instructions ##
+# Input: inputText = text gathered from web
+# Process: call atomic_find_statements(inputText)
+# Output: array of strings (possibly empty) where each string is a statement from the input text
+# Example: atomic_find_statements("Granny Smith apples are green. Do you like Granny Smith apples?") = ['granny smith apples are green']
